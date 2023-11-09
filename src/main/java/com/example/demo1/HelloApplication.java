0676bee488b8dae4076a4a95b806c99e00068cc0
Badar Ansari <ansab@farmingdale.edu>
2023-11-09 13:26:33 -0500

    Fixed some errors in the class

--- src/main/java/com/example/demo1/HelloApplication.java
+++ src/main/java/com/example/demo1/HelloApplication.java
@@ -1,19 +1,18 @@
 package com.example.demo1;
- import javafx.application.Application;
- import javafx.fxml.FXMLLoader;
- import javafx.geometry.Insets;
- import javafx.scene.Scene;
- import javafx.scene.control.*;
- import javafx.scene.layout.*;
- import javafx.stage.Stage;
 
- import java.io.IOException;
- import java.util.regex.Pattern;
- import javafx.scene.paint.Color;
- import javafx.scene.text.Font;
- import javafx.scene.text.FontWeight;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
-public class RegistrationFormApp extends Application {
+import java.util.regex.Pattern;
+
+public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         launch(args);
@@ -171,8 +170,7 @@ public class RegistrationFormApp extends Application {
         addButton.setDisable(!isValid);
     }
 
-    /**
-     * The main entry point of the application.
+    /*
      *
      * @param primaryStage The primary stage of the application.
      */
@@ -180,17 +178,6 @@ public class RegistrationFormApp extends Application {
 
 
 
-        public class HelloApplication extends Application {
-            @Override
-            public void start(Stage primaryStage) throws IOException {
-                FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-                Scene scene = new Scene(fxmlLoader.load(), 320, 240);
 
-            }
-
-            public static void main(String[] args) {
-                launch();
-            }
         }
-    }
-}
+
